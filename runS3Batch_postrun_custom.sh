f97f4c3f87fc42db9bd1f441be2214f7a8c10e25
ted Liefeld <liefeld@broadinstitute.org>
2018-01-10 09:18:46 -0800

    updating copyright

--- runS3Batch_postrun_custom.sh
+++ runS3Batch_postrun_custom.sh
@@ -1,4 +1,5 @@
+# copyright 2017-2018 Regents of the University of California and the Broad Institute. All rights reserved.
 
-echo "R2.7 POST RUN CUSTOM: PERFORMING aws s3 sync $R_LIBS $S3_ROOT$R_LIBS_S3"
+echo "R2.8 POST RUN CUSTOM: PERFORMING aws s3 sync $R_LIBS $S3_ROOT$R_LIBS_S3"
 aws s3 sync $R_LIBS $S3_ROOT$R_LIBS_S3 --quiet
 
